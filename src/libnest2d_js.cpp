e137273a31f1aed76570b6f80954da52420b0f65
saumya.jain <saumya.jain@ultimaker.com>
2025-09-11 15:09:02 +0200

    Update JS/WASM bindings to reflect template-based types in libnest2d with ClipperLib and opt:: integration
    NP-1172

--- src/libnest2d_js.cpp
+++ src/libnest2d_js.cpp
@@ -14,37 +14,36 @@ using namespace libnest2d;
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(libnest2d_js) {
-    // Expose Point type
-    value_object<Point>("Point")
-        .field("x", &Point::x)
-        .field("y", &Point::y);
+    // Expose ClipperLib::IntPoint as Point
+    value_object<ClipperLib::IntPoint>("Point")
+        .field("x", &ClipperLib::IntPoint::X)
+        .field("y", &ClipperLib::IntPoint::Y);
 
-    // Expose Polygon type
-    value_object<Polygon>("Polygon")
-        .field("points", &Polygon::points);
+    // Expose ClipperLib::Polygon
+    value_object<ClipperLib::Polygon>("Polygon")
+        .field("contour", &ClipperLib::Polygon::Contour)
+        .field("holes", &ClipperLib::Polygon::Holes);
 
-    // Expose NesterConfig
-    value_object<NesterConfig>("NesterConfig")
-        .field("iterations", &NesterConfig::iterations)
-        .field("time_limit", &NesterConfig::time_limit);
+    // Expose NestConfig (template, so use default types)
+    value_object<NestConfig<>>("NestConfig")
+        .field("placer_config", &NestConfig<>::placer_config);
 
-    // Expose Nester class
-    class_<Nester>("Nester")
+    // Expose _Nester (template, so use default types)
+    class_<_Nester<NfpPlacer, FirstFitSelection>>("Nester")
         .constructor<>()
-        .function("nest", &Nester::nest);
+        .function("nest", &_Nester<NfpPlacer, FirstFitSelection>::nest);
 
-    // Expose OptimizerConfig
-    value_object<OptimizerConfig>("OptimizerConfig")
-        .field("max_evals", &OptimizerConfig::max_evals)
-        .field("tolerance", &OptimizerConfig::tolerance);
+    // Expose StopCriteria as OptimizerConfig
+    value_object<opt::StopCriteria>("OptimizerConfig")
+        .field("max_iterations", &opt::StopCriteria::max_iterations)
+        .field("absolute_score_difference", &opt::StopCriteria::absolute_score_difference)
+        .field("relative_score_difference", &opt::StopCriteria::relative_score_difference)
+        .field("stop_score", &opt::StopCriteria::stop_score);
 
-    // Expose Optimizer class
-    class_<Optimizer>("Optimizer")
-        .constructor<>()
-        .function("optimize", &Optimizer::optimize);
-
-    // Expose utility functions (example)
-    function("polygonArea", &polygon_area);
+    // Expose Optimizer (template, so use NloptOptimizer)
+    class_<opt::Optimizer<opt::NloptOptimizer>>("Optimizer")
+        .constructor<const opt::StopCriteria&>()
+        .function("optimize_min", &opt::Optimizer<opt::NloptOptimizer>::optimize_min<double>);
 }
 
 #endif // LIBNEST2D_JS_H
