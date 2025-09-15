a989e195cf347459e026b32a123ad8f9e5ad15b3
saumya.jain <saumya.jain@ultimaker.com>
2025-09-15 11:53:48 +0200

    Enhance `libnest2d_js` bindings: register vector and core types, add `nestItems` wrapper for improved usability.

--- libnest2d_js/libnest2d_js.cpp
+++ libnest2d_js/libnest2d_js.cpp
@@ -1,4 +1,3 @@
-// Expose libnest2d API to JavaScript via Emscripten
 #ifndef LIBNEST2D_JS_H
 #define LIBNEST2D_JS_H
 
@@ -14,19 +13,29 @@ using namespace libnest2d;
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(libnest2d_js) {
+    // Register vector types for JS
+    register_vector<Item>("ItemVector");
+    register_vector<Box>("BoxVector");
+    register_vector<PackGroup>("PackGroupVector");
+    register_vector<Coord>("CoordVector");
 
-    // Expose nest function from libnest2d
-    function("nest",
-        select_overload<std::size_t(std::vector<Item>::iterator,
-                                    std::vector<Item>::iterator,
-                                    const Box&,
-                                    Coord,
-                                    const NestConfig<NfpPlacer, FirstFitSelection>&,
-                                    NestControl)>(
-            &nest<NfpPlacer, FirstFitSelection, std::vector<Item>::iterator>
-        )
-    );
+    // Register main types
+    emscripten::register_type<Box>("Box");
+    emscripten::register_type<NestConfig<NfpPlacer, FirstFitSelection>>("NestConfig");
+    emscripten::register_type<NestControl>("NestControl");
 
+    emscripten::register_optional<Box>();
+    emscripten::register_optional<NestConfig<NfpPlacer, FirstFitSelection>>();
+    emscripten::register_optional<NestControl>();
+
+    // Provide a wrapper for nest that takes a vector<Item> directly
+    function("nestItems", optional_override([](std::vector<Item>& items,
+                                               const Box& bin,
+                                               Coord spacing,
+                                               const NestConfig<NfpPlacer, FirstFitSelection>& config,
+                                               NestControl control) {
+        return nest<NfpPlacer, FirstFitSelection>(items.begin(), items.end(), bin, spacing, config, control);
+    }));
 }
 
 #endif // LIBNEST2D_JS_H
