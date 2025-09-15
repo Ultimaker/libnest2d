fd2f888214a8626d54201e2860ca2617f3663458
saumya.jain <saumya.jain@ultimaker.com>
2025-09-15 09:48:09 +0200

    Remove unused type bindings and simplify `libnest2d_js` exports.

--- libnest2d_js/libnest2d_js.cpp
+++ libnest2d_js/libnest2d_js.cpp
@@ -14,67 +14,19 @@ using namespace libnest2d;
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(libnest2d_js) {
-    // Expose ClipperLib::IntPoint as Point
-    value_object<ClipperLib::IntPoint>("Point")
-        .field("x", &ClipperLib::IntPoint::X)
-        .field("y", &ClipperLib::IntPoint::Y);
 
-    register_vector<ClipperLib::IntPoint>("IntPointVector");
-    register_vector<std::vector<ClipperLib::IntPoint>>("IntPointVectorVector");
-    register_vector<ClipperLib::Polygon>("PolygonVector");
-    register_vector<std::vector<ClipperLib::Polygon>>("PolygonVectorVector");
-    register_vector<Item>("ItemVector");
-    register_vector<Box>("BoxVector");
-    // Expose ClipperLib::Polygon
-    value_object<ClipperLib::Polygon>("Polygon")
-        .field("contour", &ClipperLib::Polygon::Contour)
-        .field("holes", &ClipperLib::Polygon::Holes);
+    // Expose nest function from libnest2d
+    function("nest",
+        select_overload<std::size_t(std::vector<Item>::iterator,
+                                    std::vector<Item>::iterator,
+                                    const Box&,
+                                    Coord,
+                                    const NestConfig<NfpPlacer, FirstFitSelection>&,
+                                    NestControl)>(
+            &nest<NfpPlacer, FirstFitSelection, std::vector<Item>::iterator>
+        )
+    );
 
-    // Expose NestConfig (template, so use default types)
-    value_object<NestConfig<>>("NestConfig")
-        .field("placer_config", &NestConfig<>::placer_config);
-
-        // Expose nest function from libnest2d
-        function("nest",
-            select_overload<std::size_t(std::vector<Item>::iterator,
-                                        std::vector<Item>::iterator,
-                                        const Box&,
-                                        Coord,
-                                        const NestConfig<NfpPlacer, FirstFitSelection>&,
-                                        NestControl)>(
-                &nest<NfpPlacer, FirstFitSelection, std::vector<Item>::iterator>
-            )
-        );
-
-    // Expose StopCriteria as OptimizerConfig
-    value_object<opt::StopCriteria>("OptimizerConfig")
-        .field("max_iterations", &opt::StopCriteria::max_iterations)
-        .field("absolute_score_difference", &opt::StopCriteria::absolute_score_difference)
-        .field("relative_score_difference", &opt::StopCriteria::relative_score_difference)
-        .field("stop_score", &opt::StopCriteria::stop_score);
-
-    // Expose Optimizer (template, so use NloptOptimizer)
-    class_<opt::Optimizer<opt::NloptOptimizer>>("Optimizer")
-        .constructor<const opt::StopCriteria&>()
-        .function("optimize_min",
-            optional_override([](opt::Optimizer<opt::NloptOptimizer>& self,
-                                std::function<double(opt::Input<double>)> func,
-                                opt::Input<double> init,
-                                opt::Bound<double> bound) {
-                return self.optimize_min(func, init, bound);
-            })
-        );
-
-    // Bind Item<ClipperLib::Polygon>
-    class_<Item>("PolygonItem")
-        .constructor<const ClipperLib::Polygon&>()
-        .function("rawShape", &Item::rawShape);
-
-    // Bind Box<ClipperLib::IntPoint>
-    class_<Box>("Box")
-        .constructor<>()
-        .function("minCorner", &Box::minCorner)
-        .function("maxCorner", &Box::maxCorner);
 }
 
 #endif // LIBNEST2D_JS_H
